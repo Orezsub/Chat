f7bcf7c1e12bd5405b64a83de16ff64019b40214
Orezsub <52745664+Orezsub@users.noreply.github.com>
2020-03-19 10:39:49 +0300

    Modify UDP and TCP Connection

--- TCPConnection.py
+++ TCPConnection.py
@@ -1,14 +1,20 @@
-#									D:\Python\KSIS\Laba1\TCPConnection.py	
+#									D:\Python\KSIS\Laba2\Chat\TCPConnection.py	
+from PyQt5 import QtCore, QtWidgets
 import socket, threading, time
 
-class TCPConnection(object):
+
+class TCPConnection(QtWidgets.QWidget):
 	"""docstring for TCPConnection"""
 	def __init__(self, host, port, signal):
+		super().__init__()
 		self.host = host
 		self.port = port
+		self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 		self.client_name = ''
 		self.message_signal = signal
-		
+		self.is_server = False
+
+
 	def send(self, mtype,receiver, message):
 		self.socket.send(bytes(f'{mtype}†{receiver}†[{self.client_name}]†{message}', encoding='UTF-8'))
 
@@ -17,25 +23,69 @@ class TCPConnection(object):
 		self.socket.connect((self.host, self.port))
 		self.send('connect', 'Global', 'connected to chat')
 
-		self.shutdown = False
-		self.start_TCP_receiving()
+		self.start_TCP_receiving(None,None)
 
-	def thread_TCP_receiving(self):
+	def setsockopt_reuseaddr(self):
+		self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+	def bind(self, host, port):
+		self.socket.bind((host, port))
+
+	def listen(self, count):
+		self.socket.listen(count)
+
+	def accept(self):
+		connection, address = self.socket.accept()
+		self.start_TCP_receiving(connection, address)
+		return connection, address
+
+	def set_server_socket(self):
+		self.is_server = True
+
+	def set_client_connection_and_address(self, connection, address):
+		self.connection = connection
+		self.address = address
+
+	def get_client_connection_and_address(self):
+		return self.connection, self.address
+
+	def set_new_message(self, message):
+		self.message = message
+
+	def get_new_message(self):
+		return self.message
+
+	def set_host_and_port(self, host, port):
+		self.host = host
+		self.port = port
+
+	def set_client_name(self, client_name):
+		self.client_name = client_name
+
+	def thread_TCP_receiving(self, name, connection, address):
 		while not self.shutdown:
 			try:
 				while not self.shutdown:
-					data = self.socket.recv(1024).decode("utf-8").split('†')
+					if self.is_server:
+						data = connection.recv(1024).decode("utf-8").split('†')
+						self.set_client_connection_and_address(connection, address)
+
+					else:
+						data = self.socket.recv(1024).decode("utf-8").split('†')
 						
-					self.message = data
-					# print(data)
+					print(data)
+					self.set_new_message(data)
 					self.message_signal.emit()
-		
+
 					time.sleep(0.2)
-			except:
+			except ConnectionResetError:
 				pass
 
-	def start_TCP_receiving(self):
-		Thread_recv_TCP = threading.Thread(target=self.thread_TCP_receiving, daemon = True)
+	def start_TCP_receiving(self, connection, address):
+		self.shutdown = False
+		
+		Thread_recv_TCP = threading.Thread(target=self.thread_TCP_receiving,
+									args=("Client", connection, address), daemon = True)			
 		Thread_recv_TCP.start()
 
 	def disconnect(self):
