7b1bd6b9f921fd06e8f77a7a5510f8acbd5e9bde
Orezsub <52745664+Orezsub@users.noreply.github.com>
2020-03-20 03:12:47 +0300

    Completely rewriten client and server parts

--- chat_client.py
+++ chat_client.py
@@ -1,284 +1,280 @@
-#									D:\Python\KSIS\Laba1\chat_client.py					
+#									D:\Python\KSIS\Laba2\Chat\chat_client.py					
 import sys  # sys нужен для передачи argv в QApplication
 from PyQt5 import Qt, QtCore, QtGui, QtWidgets, uic
 from PyQt5.QtCore import pyqtSignal, QObject
-from design import Ui_MainWindow  
-
-def connect_or_disconnect(sender, receiver, client_name):
-	if receiver == 'connect':
-		clients.append(sender)
-		application.ui.ComboBox_Of_Clients.addItem(client_name)
-		application.added_connection_address = sender
-		application.added_connection_name = client_name
-		application.signal.Add_Dialog.emit()
-
-	elif receiver == 'disconnect':
-		application.ui.ComboBox_Of_Clients.removeItem(clients.index(sender))
-		clients.remove(sender)
-		application.delited_connection_address = sender
-		application.signal.Del_Dialog.emit()
-
-
-def change_color(sender, receiver):
-	if receiver != GLOBAL:
-		for but, addr in button_dict.items():
-			if sender == addr and application.addr_to_send != addr:
-				change_dialog_button_color(but, RED_COLOR)	
-
-	elif receiver == GLOBAL and application.addr_to_send != GLOBAL:
-		for but, addr in button_dict.items():
-			if GLOBAL == addr:
-				change_dialog_button_color(but, RED_COLOR)
-
-
-def check_history_request(data, message):
-	if data[0] == 'history start':
-		for i in reversed(range(len(message_list))):
-			if message_list[i][1] == GLOBAL:
-				del message_list[i]
-		return True
-	elif data[1] == 'history':
-		message_list.append([data[0],GLOBAL,message])
-		return True
-		
-	elif data[0] == 'history end':
-		application.signal.Clear_TEdit.emit()				
-		return True
-		
-	elif data[0] == 'active clients': 
-		if len(data) != 2:
-			for i in range(1,len(data),2):
-				clients.append(data[i+1])
-				application.ui.ComboBox_Of_Clients.addItem(data[i])
-		return True
-	return False
-
-
-def new_message():
-	# print('-->',TCP_socket.message)
-	data = TCP_socket.message
-	try:
-		sender = data[0]
-		receiver = data[1]
-		client_name = data[2]
-		message = data[2]+'†'.join(data[3:])
-	except:
-		message = ''
-
-	if not check_history_request(data, message):
-	
-		connect_or_disconnect(sender, receiver, client_name)
-
-		if receiver == 'connect' or receiver == 'disconnect':
-			receiver = GLOBAL
-
-		append_text(sender, receiver, message)
-
-		add_dialog_if_no_such(sender, client_name)
-
-		message_list.append([sender, receiver, message])
-		
-		time.sleep(0.2)
-		change_color(sender, receiver)	
-
-
-def add_dialog_button_if_no_such(address, client_name):
-	is_in = False
-	for but, addr in button_dict.items():
-		if address == addr:
-			is_in = True
-			return but
-	if not is_in:
-		button = Qt.QPushButton('{}'.format(client_name))
-		button.setStyleSheet(f'background : {BASE_COLOR};')
-
-		Vertical_layout.addWidget(button)
-		button_dict[button] = address
-		button.clicked.connect(application.buttonClicked)
-
-		return button
-
-
-def add_dialog():
-	address = application.added_connection_address
-	client_name = application.added_connection_name
-	add_dialog_button_if_no_such(address, client_name)
-
-
-def del_dialog():
-	for but, addr in button_dict.items():
-		if application.delited_connection_address == addr:	
-			but.setParent(None)
-
-
-def clear_TEdit():
-	add_message_into_TEdit(GLOBAL)
-
-
-def history_request():
-	send_message_to_server('request',GLOBAL, 'history request')
-
-
-def append_text(sender, receiver, message):
-	if receiver == GLOBAL:
-		if application.addr_to_send == GLOBAL:
-			application.ui.TEdit_Chat_Text.append(message)
-	else :
-		if application.addr_to_send == sender:
-			application.ui.TEdit_Chat_Text.append(message)
-
-
-def add_dialog_if_no_such(address, client_name):
-	is_in = False
-	for but, addr in button_dict.items():
-		if address == addr:
-			is_in = True
-			break
-	if not is_in:
-		application.added_connection_address = address
-		application.added_connection_name = client_name
-		application.signal.Add_Dialog.emit()
-
-
-def find_server():	
-	UDP_socket.start_UDP_receiving()
-	UDP_socket.start_UDP_sending()
-
-	time.sleep(0.05)
-	application.ui.Edit_IP.setText(UDP_socket.finded_ip)
-	application.ui.Edit_Port.setText(str(50007))
-
-
-def send_message_to_server(mtype, receiver, message):	
-	if message != 'history request':
-		time_now = strftime("%H:%M:%S %d-%m-%Y", gmtime())
-		mes = f' |{time_now}| {message}'
-		message_list.append(['-->',receiver,mes])
-	TCP_socket.send(mtype, receiver, message)
-	
-
-def log_in():
-	application.ui.Btn_Log_In.setDisabled(True)
-	application.ui.Btn_Log_Out.setDisabled(False)
-
-	TCP_socket.host = application.ui.Edit_IP.text()
-	TCP_socket.port = int(application.ui.Edit_Port.text())
-
-	TCP_socket.client_name = application.ui.Edit_Name.text()
-	TCP_socket.connect()
-	
-
-def prepare_and_send_message():	
-	message = application.ui.TEdit_Input_Message.toPlainText()
-	time_now = strftime("%H:%M:%S %d-%m-%Y", gmtime())
-	mes = f' |{time_now}| {message}'
-
-	if message:
-		application.ui.TEdit_Input_Message.clear()
-		send_message_to_server('message', application.addr_to_send, message)
-		application.ui.TEdit_Chat_Text.append(f'--> {mes}')
-		application.ui.TEdit_Input_Message.setFocus()
-
-
-def close_connection():
-	try:
-		TCP_socket.disconnect()
-	except:
-		pass
-
-
-def close_all_dialogs():
-	for dialog, addr in button_dict.items():
-		if addr != GLOBAL:
-			dialog.setParent(None)
-		else:
-			save_dialog = dialog
-			save_addr = addr
-	button_dict.clear()
-	button_dict[save_dialog] = save_addr
-
-
-def log_out():
-	application.ui.Btn_Log_In.setDisabled(False)
-	application.ui.Btn_Log_Out.setDisabled(True)
-	close_connection()
-	close_all_dialogs()
-	for i in range(1, application.ui.ComboBox_Of_Clients.count()):
-		application.ui.ComboBox_Of_Clients.removeItem(i)
-		del clients[i]
-	message_list.clear()
-	
-
-def add_message_into_TEdit(receiver_address):
-	# message[0] - sender, message[1] - receiver, message[2] - message_text
-	application.ui.TEdit_Chat_Text.clear()
-	if receiver_address == GLOBAL:					
-		for message in message_list:
-			if message[1] == receiver_address:
-				if message[0] == '-->':
-					application.ui.TEdit_Chat_Text.append(f'{message[0]} {message[2]}')
-				else:
-					application.ui.TEdit_Chat_Text.append(f'{message[2]}')
-	else:
-		for message in message_list:
-			if message[1] == receiver_address:
-				if message[0] == '-->':
-					application.ui.TEdit_Chat_Text.append(f'{message[0]} {message[2]}')
-				else:
-					application.ui.TEdit_Chat_Text.append(f'{message[2]}')
-			elif message[0] == receiver_address and message[1] != GLOBAL:
-				application.ui.TEdit_Chat_Text.append(f'{message[2]}')
-
-
-def change_active_dialog():
-	name = application.ui.ComboBox_Of_Clients.currentText()
-	address = clients[application.ui.ComboBox_Of_Clients.currentIndex()]
-
-	button = add_dialog_button_if_no_such(address, name)
-	button.click()
-
-
-def change_dialog_button_color(button, color):
-	button.setStyleSheet(f'background : {color};')
+from client_design import Ui_MainWindow  
 
 
 class Communicate(QObject):
-	Add_Dialog = pyqtSignal()
-	Del_Dialog = pyqtSignal()
-	Clear_TEdit = pyqtSignal()
 	new_message = pyqtSignal()
 
 
 class MainWindow(QtWidgets.QMainWindow):
+	
 	def __init__(self):
 		super(MainWindow, self).__init__()
+
 		self.ui = Ui_MainWindow()
-		self.ui.setupUi(self)
+		self.ui.setupUi(self)		
+
+		self.GLOBAL = 'Global'
+		self.CONNECT = 'connect'
+		self.DISCONNECT = 'disconnect'
+		self.ARROW = '-->'
+
+		self.BASE_COLOR = '#F0F0F0'
+		self.RED_COLOR = '#DB4747'
+
+		self.clients = [self.GLOBAL]
+		self.button_dict = {}
+		self.message_list = []
 		
 		self.signal = Communicate()
-		self.signal.new_message.connect(new_message)
-		self.signal.Add_Dialog.connect(add_dialog)
-		self.added_connection_address = ''
-		self.added_connection_name = ''		
-		self.signal.Del_Dialog.connect(del_dialog)
-		self.delited_connection_address = ''
-		self.addr_to_send = GLOBAL
-		self.ui.ComboBox_Of_Clients.currentIndexChanged.connect(change_active_dialog)
-		self.signal.Clear_TEdit.connect(clear_TEdit)
+		self.signal.new_message.connect(self.new_message)
+		self.recipient_address = self.GLOBAL
+
+		self.Vertical_layout = self.ui.verticalLayout
+		self.add_dialog_button_if_no_such(self.GLOBAL, self.GLOBAL)
+		
+		Widget = Qt.QWidget()
+		Widget.setLayout(self.Vertical_layout)
+
+		Scroll_Area = self.ui.scrollArea
+		Scroll_Area.setWidget(Widget)
 
 	def buttonClicked(self):
 		sender = self.sender()
-		# print(str(sender))
-		for but, addr in button_dict.items():
+		for but, addr in self.button_dict.items():
 			if sender == but:
-				application.addr_to_send = addr
-				color = but.styleSheet()
-				change_dialog_button_color(but, BASE_COLOR)						
-				add_message_into_TEdit(addr)
+				self.recipient_address = addr
+				
+				self.change_dialog_button_color(but, self.BASE_COLOR)						
+				self.display_all_messages_from_sender(addr)
 				break
+
+
+	def change_dialog_button_color(self, button, color):
+		button.setStyleSheet(f'background : {color};')
+
+
+	def find_dialog_button_for_change_color(self, sender, recipient):
+		if recipient != self.GLOBAL:
+			for but, addr in self.button_dict.items():
+				if sender == addr and self.recipient_address != addr:
+					self.change_dialog_button_color(but, self.RED_COLOR)	
+
+		elif recipient == self.GLOBAL and self.recipient_address != self.GLOBAL:
+			for but, addr in self.button_dict.items():
+				if self.GLOBAL == addr:
+					self.change_dialog_button_color(but, self.RED_COLOR)
+
+
+	def add_dialog_button_if_no_such(self, sender_address, client_name):
+		for but, addr in self.button_dict.items():
+			if sender_address == addr:
+				return but
+
+		button = Qt.QPushButton(f'{client_name}')
+		button.setStyleSheet(f'background : {self.BASE_COLOR};')
+
+		self.Vertical_layout.addWidget(button)
+		self.button_dict[button] = sender_address
+		button.clicked.connect(self.buttonClicked)
+
+		return button
 	
+
+	def append_new_message_into_chat(self, sender, recipient, message):
+		if recipient == self.GLOBAL and self.recipient_address == self.GLOBAL:
+				self.ui.TEdit_Chat_Text.append(message)
+
+		elif recipient != self.GLOBAL and self.recipient_address == sender:
+				self.ui.TEdit_Chat_Text.append(message)
+
+
+	def del_dialog_button(self, address):
+		for but, addr in self.button_dict.items():
+			if address == addr:	
+				but.setParent(None)
+
+
+	def if_any_connect_or_disconnect(self, mtype, sender, client_name):
+		if mtype == self.CONNECT:
+			self.clients.append(sender)
+			self.ui.ComboBox_Of_Clients.addItem(client_name)
+
+		elif mtype == self.DISCONNECT:
+			self.ui.ComboBox_Of_Clients.removeItem(self.clients.index(sender))
+			self.clients.remove(sender)
+			self.del_dialog_button(sender)
+
+
+	def display_all_messages_from_sender(self, recipient_address):
+		# message[0] - sender, message[1] - recipient, message[2] - message_text
+		self.ui.TEdit_Chat_Text.clear()
+		if recipient_address == self.GLOBAL:					
+			for message in self.message_list:
+				if message[1] == recipient_address:
+
+					if message[0] == self.ARROW:
+						self.ui.TEdit_Chat_Text.append(f'{message[0]} {message[2]}')
+					else:
+						self.ui.TEdit_Chat_Text.append(f'{message[2]}')
+
+		else:
+			for message in self.message_list:
+				if message[1] == recipient_address:
+
+					if message[0] == self.ARROW:
+						self.ui.TEdit_Chat_Text.append(f'{message[0]} {message[2]}')
+					else:
+						self.ui.TEdit_Chat_Text.append(f'{message[2]}')
+						
+				elif message[0] == recipient_address and message[1] != self.GLOBAL:
+					self.ui.TEdit_Chat_Text.append(f'{message[2]}')
+
+
+	def check_not_main_message(self, data):
+		if data[0] == 'history':
+			for i in reversed(range(len(self.message_list))):
+				if self.message_list[i][1] == self.GLOBAL:
+					del self.message_list[i] 
+
+			for i in range(1,len(data),5):
+				self.message_list.append([data[i+1],data[i+2],f'{data[i+3]} :: {data[i+4]}'])
+
+			for but, addr in self.button_dict.items():
+				if addr == self.GLOBAL:
+					but.click()
+			return True
+			
+		elif data[0] == 'active_clients': 
+			if len(data) != 2:
+				for i in range(1,len(data),2):
+					self.ui.ComboBox_Of_Clients.addItem(data[i])
+					self.clients.append(data[i+1])
+			return True
+
+		return False
+
+
+	def new_message(self):
+		data = self.TCP_socket.get_new_message()
+
+		if not self.check_not_main_message(data):
+			mtype = data[0]
+			sender = data[1]
+			recipient = data[2]
+			client_name = data[3]
+			message = data[3]+'†'.join(data[4:])
+
+			self.if_any_connect_or_disconnect(mtype, sender, client_name)
+			self.append_new_message_into_chat(sender, recipient, message)
+			self.add_dialog_button_if_no_such(sender, client_name)
+
+			self.message_list.append([sender, recipient, message])
+			
+			time.sleep(0.2)
+			self.find_dialog_button_for_change_color(sender, recipient)	
+
+
+	def find_server(self):
+		client_host = socket.gethostbyname(socket.gethostname())
+		client_port = 50007
+
+		UDP_socket = UDPConnection(client_host, client_port)
+		UDP_socket.setsockopt_broadcast()
+
+		UDP_socket.start_UDP_receiving()
+		UDP_socket.start_UDP_sending()
+
+		time.sleep(0.05)
+		host, port = UDP_socket.get_finded_ip_and_port()
+		self.ui.Edit_IP.setText(host)
+		self.ui.Edit_Port.setText(str(port))
+		UDP_socket.stop()	
+
+
+	def log_in(self):
+		self.ui.Btn_Log_In.setDisabled(True)
+		self.ui.Btn_Log_Out.setDisabled(False)
+		self.ui.Btn_Send_Message.setDisabled(False)
+		self.ui.Btn_History_Request.setDisabled(False)
+
+		host = self.ui.Edit_IP.text()
+		port = int(self.ui.Edit_Port.text())
+
+		self.TCP_socket.set_host_and_port(host, port)
+		self.TCP_socket.set_client_name(self.ui.Edit_Name.text())
+
+		self.TCP_socket.connect()
+
+
+	def prepare_and_send_message(self):	
+		message = self.ui.TEdit_Input_Message.toPlainText()
+		if message:
+			self.send_message_to_server('message', self.recipient_address, message, True)
+	
+
+	def send_message_to_server(self, mtype, recipient, message, print_message):
+		time_now = strftime("%H:%M:%S %d-%m-%Y", gmtime())
+		mes = f' |{time_now}| {message}'
+
+		if mtype != 'history_request':			
+			self.message_list.append([self.ARROW, recipient, mes])
+		self.TCP_socket.send(mtype, recipient, message)
+
+		if print_message:
+			self.ui.TEdit_Input_Message.clear()
+			self.ui.TEdit_Chat_Text.append(f'--> {mes}')
+			self.ui.TEdit_Input_Message.setFocus()
+	
+
+	def log_out(self):
+		self.ui.Btn_Log_In.setDisabled(False)
+		self.ui.Btn_Log_Out.setDisabled(True)
+		self.ui.Btn_Send_Message.setDisabled(True)
+		self.ui.Btn_History_Request.setDisabled(True)
+
+		self.close_connection()
+		self.close_all_dialogs()
+		for i in range(1, self.ui.ComboBox_Of_Clients.count()):
+			self.ui.ComboBox_Of_Clients.removeItem(i)
+			del self.clients[i]
+		self.message_list.clear()
+			
+
+	def close_connection(self):
+		self.TCP_socket.disconnect()
+
+	def close_all_dialogs(self):
+		for dialog, addr in self.button_dict.items():
+			if addr != self.GLOBAL:
+				dialog.setParent(None)
+			else:
+				save_dialog = dialog
+				save_addr = addr
+		self.button_dict.clear()
+		self.button_dict[save_dialog] = save_addr
+
+
+	def history_request(self):
+		self.send_message_to_server('history_request', self.GLOBAL, 'history request', False)
+
+
+	def change_active_dialog(self):
+		name = self.ui.ComboBox_Of_Clients.currentText()
+		address = self.clients[self.ui.ComboBox_Of_Clients.currentIndex()]
+
+		button = self.add_dialog_button_if_no_such(address, name)
+		button.click()
+
+
+	def set_TCP_socket(self, socket):
+		self.TCP_socket = socket
+
 	def closeEvent(self, event): 
-		close_connection()
+		self.close_connection()
 		
 
 
@@ -291,43 +287,21 @@ if __name__ == "__main__":
 	from TCPConnection import TCPConnection	
 	from time import gmtime, strftime
 
-	GLOBAL = 'Global'
-
-	BASE_COLOR = '#F0F0F0'
-	RED_COLOR = '#DB4747'	
 
 	app = QtWidgets.QApplication(sys.argv)
 	application = MainWindow()
 	application.show()	
-
-	HOST = socket.gethostbyname(socket.gethostname())
-	PORT = 50007
 	
-	UDP_socket = UDPConnection(HOST, PORT)
-	TCP_socket = TCPConnection(HOST, PORT, application.signal.new_message)
 
- 
-	application.ui.Btn_Find_Server.clicked.connect(find_server)
-	application.ui.Btn_Log_In.clicked.connect(log_in)
-	application.ui.Btn_Send_Message.clicked.connect(prepare_and_send_message)
-	application.ui.Btn_Log_Out.clicked.connect(log_out)
-	application.ui.Btn_History_Request.clicked.connect(history_request)
+	TCP_socket = TCPConnection(application.signal.new_message)
+	application.set_TCP_socket(TCP_socket)
 
-	clients = [GLOBAL]
-	button_dict = {}
-	message_list = []	
-
-	Vertical_layout = application.ui.verticalLayout
-	add_dialog_button_if_no_such(GLOBAL,GLOBAL)
-
-	
-	Widget = Qt.QWidget()
-	Widget.setLayout(Vertical_layout)
-
-
-	Scroll_Area = application.ui.scrollArea
-	Scroll_Area.setWidget(Widget)
+	application.ui.Btn_Find_Server.clicked.connect(application.find_server)
+	application.ui.Btn_Log_In.clicked.connect(application.log_in)
+	application.ui.Btn_Send_Message.clicked.connect(application.prepare_and_send_message)
+	application.ui.Btn_Log_Out.clicked.connect(application.log_out)
+	application.ui.Btn_History_Request.clicked.connect(application.history_request)
+	application.ui.ComboBox_Of_Clients.currentIndexChanged.connect(application.change_active_dialog)
 
 
 	sys.exit(app.exec_())
-
--- chat_server.py
+++ chat_server.py
@@ -1,106 +1,131 @@
-#									D:\Python\KSIS\Laba1\test_server.py	
-import socket, threading, time
-from time import gmtime, strftime
+#									D:\Python\KSIS\Laba2\Chat\chat_server.py	
+from PyQt5.QtCore import pyqtSignal, QObject
+from PyQt5 import QtCore, QtWidgets
+from server_design import Ui_MainWindow  
 
-host = socket.gethostbyname(socket.gethostname())
-port = 50007
 
-UDP_socket=socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-UDP_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-UDP_socket.bind(('0.0.0.0', port)) 
+class Communicate(QObject):
+	new_mes = pyqtSignal()
 
-TCP_socket=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-TCP_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-TCP_socket.bind((host, port)) 
-TCP_socket.listen(10)
+class MainWindow(QtWidgets.QWidget):
+	def __init__(self):
+		super(MainWindow, self).__init__()
+		
+		self.ui = Ui_MainWindow()
+		self.ui.setupUi(self)
 
-clients = {}
-message_list = []
-names_list = []
+		self.GLOBAL = 'Global'
+		self.CONNECT = 'connect'
+		self.DISCONNECT = 'disconnect'
+		self.MESSAGE = 'message'
+		self.HISTORY_REQUST = 'history_request'
+		self.ACTIVE_CLIENTS = 'active_clients'
 
-def UDP_receiving(name, sock):  # Поточность
-	while True:
-		try:
-			while True:
-				data, addr = UDP_socket.recvfrom(1024)
-				print(data) 
-				print(addr)
-				UDP_socket.sendto((str(host)+' '+str(port)).encode("utf-8"), addr)
-				time.sleep(0.2)
-		except:
-			pass
+		self.clients = {}
+		self.message_list = []
+		self.client_id_and_name = {}
 
-Thread_recv_UDP = threading.Thread(target=UDP_receiving, args=("RecvThread", UDP_socket), daemon = True)
-Thread_recv_UDP.start()
+		self.signal = Communicate()
+		self.signal.new_mes.connect(self.new_mes)
 
-def Data_processing(mtype, receiver, client_name, data, addr, ip):
-	time_now = strftime("%H:%M:%S %d-%m-%Y", gmtime())
-
-	mes = '†'.join(data)
-	message = f' |{time_now} {ip}:{port}| {mes}'
-	if receiver == 'Global' and mtype != 'request':
-		message_list.append(f'{addr}†history†{client_name}†{message}')
-	if mtype == 'connect':
-		return f'{addr}†{receiver}†{client_name}† {message}'
-	elif mtype == 'message':
-		return f'{addr}†{receiver}†{client_name}† :: {message}'
-	elif mtype == 'disconnect':
-		return f'{addr}†{receiver}†{client_name}† {message}'
-
-def Send_Message_To_Client(message, receiver, connection):
-	for client, addr in clients.items():
-		if connection != client and (addr == receiver or receiver == 'Global'
-									or receiver == 'connect'or receiver == 'disconnect'):
-			client.send(bytes(message.encode('utf-8')))
-
-def TCP_receiving(name, connection, address):  # Поточность
-	shutdown = False
-	while not shutdown:
-		# try:
-		while not shutdown:
-			data = connection.recv(1024).decode('utf-8').split('†')
-			print(data)
-
-			message = Data_processing(data[0],data[1],data[2],data[3:], str(address[1]), address[0])
-
-			if data[0] == 'disconnect':
-				# print(clients[connection])
-				del names_list[names_list.index(clients[connection])-1]
-				del names_list[names_list.index(clients[connection])]
-				del clients[connection]
-				message = Data_processing(data[0],data[0],data[2],data[3:], str(address[1]), address[0])
-				# connection.shutdown(socket.SHUT_RDWR)
-				shutdown = True	
-			elif data[0] == 'connect':
-				connection.send(bytes(('active clients†'+'†'.join(names_list)).encode('utf-8')))
-				names_list.append(data[2])
-				names_list.append(str(address[1]))
-				message = Data_processing(data[0],data[0],data[2],data[3:], str(address[1]), address[0])
-			elif data[0] == 'request':
-				connection.send(bytes('history start† † † '.encode('utf-8')))
-				for message in message_list:
-					connection.sendall(bytes(message.encode('utf-8')))
-					print(message)
-					time.sleep(0.2)
-				connection.send(bytes('history end† † † '.encode('utf-8')))
-				continue
-
+	def process_not_main_message(self, mtype, client_name, client_id, connection):
+		if mtype == self.DISCONNECT:
+			self.client_id_and_name.pop(self.clients[connection])
+			self.clients.pop(connection)
 			
-			
-			# print(message)
+		elif mtype == self.CONNECT:
+			active_clients = ''
+			for _id, _name in self.client_id_and_name.items():
+				active_clients += f'†{_name}†{_id}'
 
-			Send_Message_To_Client(message, data[1], connection)
-			# print(connection)
-			print(message,' ',address)
-			
-			time.sleep(0.2)
-		# except:
-		# 	pass
+			connection.send(bytes(f'active_clients{active_clients}'.encode('utf-8')))
+			self.client_id_and_name[client_id] = client_name
 
-while True:
-	connection, address = TCP_socket.accept()
-	print("new connection from {address}".format(address=address))
-	clients[connection] = str(address[1])
-	Thread_recv_TCP = threading.Thread(target=TCP_receiving, args=("RecvThread", connection, address), daemon = True)
-	Thread_recv_TCP.start()
+		elif mtype == self.HISTORY_REQUST:
+			history = '†'.join(self.message_list)
+			connection.sendall(bytes(f'history†{history}'.encode('utf-8')))
 
+
+	def new_mes(self):
+		data = self.socket.get_new_message()
+		connection, address = self.socket.get_client_connection_and_address()
+
+		mtype, recipient, client_name, raw_message = data[0], data[1], data[2], data[3:]
+		client_id, client_ip = str(address[1]), address[0]
+
+		message = self.message_constructor(mtype, recipient, client_name, raw_message, client_id, client_ip)
+		self.process_not_main_message(mtype, client_name, client_id, connection)			
+
+		if mtype != self.HISTORY_REQUST:
+			self.send_message_to_client(message, recipient, connection)
+			self.ui.TEdit_for_server_info.append(f'{message} {address}')
+	
+
+	def message_constructor(self, mtype, recipient, client_name, data, client_id, client_ip):
+		mes = '†'.join(data)		
+		message = f' |{client_ip}:{PORT}| {mes}'		
+
+		if recipient == self.GLOBAL and mtype != self.HISTORY_REQUST:
+			self.message_list.append(f'{mtype}†{client_id}†{recipient}†{client_name}†{message}')
+
+
+		if mtype == self.CONNECT or mtype == self.DISCONNECT:
+			return f'{mtype}†{client_id}†{recipient}†{client_name}† {message}'
+
+		elif mtype == self.MESSAGE:
+			return f'{mtype}†{client_id}†{recipient}†{client_name}† :: {message}'
+
+		elif mtype == self.ACTIVE_CLIENTS:
+			return f'{mtype}†{client_id}†{recipient}†{client_name}†{mes}'
+
+
+	def send_message_to_client(self, message, recipient, connection):		
+		for client, addr in self.clients.items():
+			if connection != client and (addr == recipient or recipient == self.GLOBAL):
+				client.send(bytes(message.encode('utf-8')))
+
+
+	def set_socket(self, socket):
+		self.socket = socket
+
+
+	def thread(self):
+		while True:
+			connection, address = self.socket.accept()
+			self.clients[connection] = str(address[1])
+
+
+	def start_threading(self):
+		socket_thread = threading.Thread(target=self.thread, daemon = True)
+		socket_thread.start()
+
+if __name__ == "__main__":
+	import socket, threading, time, sys
+	from time import gmtime, strftime
+	from UDPConnection import UDPConnection
+	from TCPConnection import TCPConnection	
+
+	HOST = socket.gethostbyname(socket.gethostname())
+	PORT = 50007
+	
+	app = QtWidgets.QApplication(sys.argv)
+	application = MainWindow()
+	application.show()
+
+	UDP_socket = UDPConnection(HOST, PORT)
+	UDP_socket.setsockopt_reuseaddr()
+	UDP_socket.bind('0.0.0.0', PORT) 	
+	UDP_socket.send_address_to_sender()
+	UDP_socket.start_UDP_receiving()
+		
+	TCP_socket = TCPConnection(application.signal.new_mes)
+	TCP_socket.setsockopt_reuseaddr()
+	TCP_socket.bind(HOST, PORT)
+	TCP_socket.listen(10)
+	TCP_socket.set_server_socket()
+
+	application.set_socket(TCP_socket)
+	application.start_threading()
+
+	
+	sys.exit(app.exec())
